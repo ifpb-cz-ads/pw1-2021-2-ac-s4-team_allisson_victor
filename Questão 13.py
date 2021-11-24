c12008c4b7300a2cccc51b9b157996e6503b2381
Allisson-Sousa-Lira <51099254+Allisson-Sousa-Lira@users.noreply.github.com>
2021-11-24 17:33:14 -0300

    Atividade colaborativa 4.02
    Questões de números impares

---
+++ Questão 13.py
@@ -0,0 +1,4 @@
+c = int(input("Digite a temperatura: "))
+converter = (9 * c / 5) + 32
+
+print("A temperatura em fahrenheit é %d"%converter)
---
+++ Questão 7.py
@@ -0,0 +1,5 @@
+num1 = int(input("Digite o primeiro nome: "))
+num2 = int(input("Digite o segundo nome: "))
+soma = num1 + num2
+
+print(soma)
